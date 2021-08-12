8c0375da16ce913abd5d4df3ebeba4246a5d8b66
miaodong <miaod@mail.open.com.cn>
2021-08-12 14:33:45 +0800

    1.引入 promisic 文件 代理所有的 wx.request 请求方式;

--- utils/http.js
+++ utils/http.js
@@ -1,30 +1,29 @@
-
 import { config } from "../config/Config";
+import { promisic } from "./util";
 
 /**
  * http请求的基类:
  *
  * */
 export class Http {
-    static request({ url, data, callback, method = 'GET' }) {
-        wx.request({
+    static async request({ url, data, method = 'GET' }) {
+        // 入参为函数名 , 后面接小括弧代表调用 promisic 函数 , 里面装着的是wx.request所需要的参数
+        await promisic(wx.request)({
             url: `${config.apiBaseUrl}${url}`,
             data: data,
             method: method,
-            header: {
-                appkey: config.appkey,
-            },
-            // result 里包含了 cookies、data、errMsg、header、statusCode几个字段 , data 里保存了本次请求的数据:
-            success: (result) => {
-                callback(result.data);
-            },
-            fail: () => {
-
-            },
-            complete: () => {
-
-            }
-        })
+            header: { appkey: config.appkey }
+        });
     }
 }
 
+promisic(wx.request);
+/**
+ * Home.js调用 Theme.js , Theme.js调用Http.js , 由于 Http.js 里的 wx.request 方法是异步的 ,
+ * 从而导致每一层都是异步的调用 , 需要回调;
+ * 而异步的调用有几种形式:
+ * 1.callback 回调的方式;
+ * 2.promise 方式;
+ * 3.async 和 await是最高级的方式;
+ *
+ */
--- utils/util.js
+++ utils/util.js
@@ -3,9 +3,10 @@
  * 这种方法的回调比简单的callBack好但是不如async 和await方式;
  * 这种方式是linUI框架的 promisic 方法的内部实现
  * promisic 这个方法 接收一个函数 , 返回的也是一个函数;
+ * promisic 将小程序内置的非 primise API 转换为 promise:
  * 
  * */
-const promisic = function (func) {
+ export const promisic = function (func) {
     return function (params = {}) {
         return new Promise((resolve, reject) => {
             const args = Object.assign(params, {
@@ -23,6 +24,4 @@ const promisic = function (func) {
     };
 };
 
-export {
-    promisic,
-}
+// Promise相当于用了代理模式 , 每次调用 wx.request 都是通过 promisic 代理完成的;
